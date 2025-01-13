b0ba6f0ed9bc812e980959378e6accb52b569427
webDev-Learner <pawnmugi@gmail.com>
2025-01-13 17:11:53 +0530

    Fix: Deployment changes 1

--- backend/src/index.js
+++ backend/src/index.js
@@ -6,6 +6,7 @@ import commentRoutes from "./routes/comment.route.js";
 import connectMongoDB from "./lib/db.js";
 import cookieParser from "cookie-parser";
 import cors from 'cors';
+// import path from 'path';
 
 const app = express();
 
@@ -27,6 +28,13 @@ app.use(cors({
 app.use("/api/auth",authRoutes);
 app.use("/api/blog",blogRoutes);
 app.use("/api/comment",commentRoutes);
+
+// if(process.env.NODE_ENV==="production"){
+//     app.use(express.static(path.join(__dirname, "../frontend/dist")));
+//     app.get("*",(req,res)=>{
+//         res.sendFile(path.join(__dirname, "../frontend","dist","index.html"));
+//     });
+// }
 
 const PORT = process.env.PORT;
 app.listen(PORT,()=>{
